730a8838914066e2f0cd08a690791c53c651b1df
Santiago <chago1929@hotmail.com>
2019-07-11 13:58:36 -0500

    Creacion de documentos de Word (hasta la tabla), guarda ult no.

--- funciones.py
+++ funciones.py
@@ -1,5 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
+from docx import *
+from docx import Document
+import os
 
 def test():
     window = Tk()
@@ -14,3 +17,20 @@ def test():
     x = (window.winfo_screenwidth() // 2) - (width // 2) 
     y = (window.winfo_screenheight() // 2) - (height // 2) 
     window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    
+    f = open("num.txt","r+")
+    num = int(f.readline())
+    print("Numero leido: {}".format(num))
+    f.close()
+
+    document = Document()
+    table = document.add_table(rows=23, cols=5)
+    document.save("cotizacion {}.docx".format(num))
+
+    os.remove("num.txt")
+
+    f = open("num.txt","w")
+    f.write("{}".format(num+1))
+    f.close()
+    
+
